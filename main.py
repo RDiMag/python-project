7ba618fd00125aa8ee3c417d0ccf0fa4ccda51a7
RDiMag <87457132+RDiMag@users.noreply.github.com>
2024-01-09 18:07:55 -0500

    Add search logic

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import wiki as search_wiki
+from mylib.logic import wiki as wikilogic
 
-result = wiki()
-print(result)
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call /search or /wiki"}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in Wikipedia"""
+
+    result = search_wiki(value)
+    return {"result": result}
+
+
+@app.get("/search/{name}")
+async def search(name: str):
+    """Retrieve Wikipedia page"""
+
+    result = wikilogic(name)
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
