d1a78112b5394e31e03ed5d2f4a2cd36900e86a6
RDiMag <87457132+RDiMag@users.noreply.github.com>
2024-01-10 12:36:52 -0500

    add phrase test coverage

--- test_main.py
+++ test_main.py
@@ -8,3 +8,8 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Wikipedia API. Call /search or /wiki"}
+
+def test_read_phrase():
+    response = client.get("/phrase/Barack Obama")
+    assert response.status_code == 200
+    assert response.json() == {"result": "Wikipedia API. Call /search or /wiki"}
